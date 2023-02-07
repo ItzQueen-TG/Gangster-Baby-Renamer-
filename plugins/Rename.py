bf3787de06fe8a6c7fedf7f64904999712e481f6
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-02-07 20:49:54 +0530

    Update Rename.py

--- plugins/Rename.py
+++ plugins/Rename.py
@@ -12,60 +12,9 @@ from pyrogram.types import *
 from pyrogram.errors import FloodWait
 
 
-T_CHANNEL = int(os.environ.get("T_CHANNEL", "-1001837941527"))
-F_CHANNEL = int(os.environ.get("F_CHANNEL", "-1001737494519"))
-FF_CHANNEL = int(os.environ.get("FF_CHANNEL", "-1001661692511"))
-PROGRESS_BAR = "\n\n📁 : {b} | {c}\n🚀 : {a}%\n⚡ : {d}/s\n⏱️ : {f}"
+CHANNEL = int(os.environ.get("CHANNEL", "-1001837941527"))
 
-async def progress_message(current, total, ud_type, message, start):
-    now = time.time()
-    diff = now - start
-    if round(diff % 6.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
-        progress = "\n{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
-        tmp = progress + PROGRESS_BAR.format(
-            a=round(percentage, 2),
-            b=humanbytes(current),
-            c=humanbytes(total),
-            d=humanbytes(speed),
-            f=estimated_total_time if estimated_total_time != '' else "0 s")                               
-        try:
-            await message.edit(text="{}\n{}".format(ud_type, tmp))         
-        except:
-            pass
-
-def humanbytes(size):
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
-    return "%.2f %s" % (size, units[i])
-
-def TimeFormatter(milliseconds: int) -> str:
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-          ((str(hours) + "h, ") if hours else "") + \
-          ((str(minutes) + "m, ") if minutes else "") + \
-          ((str(seconds) + "s, ") if seconds else "") + \
-          ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return tmp[:-2]
-
-
-
-@Client.on_message(filters.chat(T_CHANNEL) & (filters.document | filters.video))
+@Client.on_message(filters.chat(CHANNEL) & (filters.document | filters.video))
 async def rename_file(bot, msg):
     media = msg.document or msg.audio or msg.video
     og_media = getattr(msg, msg.media.value)
